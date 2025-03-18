c93fdda91842b4b1b0afba6479247326fafb52ee
natal <natal@192.168.0.225>
2025-03-18 08:57:11 +0100

    First commit

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php
+require_once dirname(__FILE__).'/config.php';
+
+//przekierowanie przeglądarki klienta (redirect)
+//header("Location: "._APP_URL."/app/calc_view.php");
+
+//przekazanie żądania do następnego dokumentu ("forward")
+include _ROOT_PATH.'/app/calc_view.php';
