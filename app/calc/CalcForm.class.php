11652699452721a7425497ddce786a8cfed03250
natalka-d <natalia-d100@wp.pl>
2025-04-03 12:17:31 +0200

    Delete app directory

--- app/calc/CalcForm.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-class CalcForm {
-    public $x;      // Kwota kredytu
-    public $y;      // Oprocentowanie
-    public $op;     // Okres kredytowania
-}
-?>
--- app/calc/CalcResult.class.php
+++
@@ -1,5 +0,0 @@
-<?php
-class CalcResult {
-    public $result; // Miesięczna rata
-}
-?>
